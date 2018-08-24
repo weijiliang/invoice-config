99ff1f8100f7a4c71f2ebfdde2d1d51715baa100
lightswei <lightswei@163.com>
2018-08-24 17:13:22 +0800

    config初始化

---
+++ src/main/java/com/dbic/vms/invoiceconfig/InvoiceConfigApplication.java
@@ -0,0 +1,16 @@
+package com.dbic.vms.invoiceconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class InvoiceConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceConfigApplication.class, args);
+    }
+}
